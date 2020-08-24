bea52e35776d59bd45389426cecfe1cffd13136f
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-08-24 00:03:10 -0500

    Create black_scholes_functions.py

---
+++ black_scholes_functions.py
@@ -0,0 +1,12 @@
+def d(sigma, S, K, r, t):
+    d1 = 1 / (sigma * np.sqrt(t)) * ( np.log(S/K) + (r + sigma**2/2) * t)
+    d2 = d1 - sigma * np.sqrt(t)
+    return [d1, d2]
+
+def call_price(sigma, S, K, r, t, d1, d2):
+    C = norm.cdf(d1) * S - norm.cdf(d2) * K * np.exp(-r * t)
+    return C
+
+def put_price(sigma, S, K, r, t, d1, d2):
+    P = -norm.cdf(-d1) * S + norm.cdf(-d2) * K * np.exp(-r * t)
+    return P
