006e96f2899791e834bd604d0da7a8f0fd9c6ac5
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-08-24 00:32:52 -0500

    Create td_data.py

---
+++ td_data.py
@@ -0,0 +1,13 @@
+api_key_td = ###
+
+def get_hist(**kwargs):
+    url = 'https://api.tdameritrade.com/v1/marketdata/{}/pricehistory'.format(kwargs.get('symbol'))
+    
+    params = {}
+    params.update({'apikey':api_key_td})
+    
+    for arg in kwargs:
+        parameter = {arg: kwargs.get(arg)}
+        params.update(parameter)
+        
+    return requests.get(url, params=params).json()
