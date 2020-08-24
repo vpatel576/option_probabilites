593f2efd58189df11d36ef0e41454057bdea310f
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-08-24 00:04:01 -0500

    Update options_distribution.py

--- options_distribution.py
+++ options_distribution.py
@@ -15,20 +15,7 @@ from scipy.interpolate import interp1d
 from bs4 import BeautifulSoup
 import requests
 
-#Black-Scholes functions
-
-def d(sigma, S, K, r, t):
-    d1 = 1 / (sigma * np.sqrt(t)) * ( np.log(S/K) + (r + sigma**2/2) * t)
-    d2 = d1 - sigma * np.sqrt(t)
-    return [d1, d2]
-
-def call_price(sigma, S, K, r, t, d1, d2):
-    C = norm.cdf(d1) * S - norm.cdf(d2) * K * np.exp(-r * t)
-    return C
-
-def put_price(sigma, S, K, r, t, d1, d2):
-    P = -norm.cdf(-d1) * S + norm.cdf(-d2) * K * np.exp(-r * t)
-    return P
+from black_scholes_function import d,call_price,put_price
 
 #Inputs for company ticker and Puts or Calls
 ticker = input('Ticker: ')
