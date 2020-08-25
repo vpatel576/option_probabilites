ad0167c43c8db47c119ab3fd056b54704caa6dbe
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-08-24 23:58:21 -0500

    Update td_data.py

--- td_data.py
+++ td_data.py
@@ -11,24 +11,3 @@ def get_hist(**kwargs):
         params.update(parameter)
         
     return requests.get(url, params=params).json()
-
-#Grabbing options data
-
-data = get_hist(symbol = 'T',contractType='ALL',strikeCount=10)
-dates = list(data['callExpDateMap'].keys())
-strikes = list(data['callExpDateMap'][dates[0]].keys())
-
-strikes = []
-vol = []
-
-for d in dates:
-    s = list(data['callExpDateMap'][d].keys())
-    v = []
-    for str in s:
-        v.append(data['callExpDateMap'][d][str][0]['volatility'])
-    
-    v = [float(i) for i in v]
-    s = [float(i) for i in s]
-    
-    strikes.append(s)
-    vol.append(v)
