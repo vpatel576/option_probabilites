2e77c5306fe7bf9e49df1afdb9c8e93d6553f3a6
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-08-24 23:56:46 -0500

    Update td_data.py

--- td_data.py
+++ td_data.py
@@ -1,7 +1,7 @@
 api_key_td = ###
 
 def get_hist(**kwargs):
-    url = 'https://api.tdameritrade.com/v1/marketdata/{}/pricehistory'.format(kwargs.get('symbol'))
+    url ='https://api.tdameritrade.com/v1/marketdata/chains'
     
     params = {}
     params.update({'apikey':api_key_td})
@@ -11,3 +11,24 @@ def get_hist(**kwargs):
         params.update(parameter)
         
     return requests.get(url, params=params).json()
+
+#Grabbing options data
+
+data = get_hist(symbol = 'T',contractType='ALL',strikeCount=10)
+dates = list(data['callExpDateMap'].keys())
+strikes = list(data['callExpDateMap'][dates[0]].keys())
+
+strikes = []
+vol = []
+
+for d in dates:
+    s = list(data['callExpDateMap'][d].keys())
+    v = []
+    for str in s:
+        v.append(data['callExpDateMap'][d][str][0]['volatility'])
+    
+    v = [float(i) for i in v]
+    s = [float(i) for i in s]
+    
+    strikes.append(s)
+    vol.append(v)
